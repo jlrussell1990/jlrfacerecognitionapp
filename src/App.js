18900affdcf6a66f87c8bb2a4aaa09907021cc14
jlrussell1990 <jlrussell1990@gmail.com>
2023-09-02 11:11:46 -0500

    button change

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
     });
   };
 
-  onSubmitSignin = () => {
+  onButtonSubmit = () => {
   console.log('Button clicked!'); // Add this line
   console.log('Current state:', this.state); // Add this line
 
